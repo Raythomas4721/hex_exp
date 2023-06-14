c38cd5dedd45cc47dccae97cdcd404e1a5aeefac
Ray <pcheng25@student.mtsac.edu>
2023-06-14 01:23:13 -0700

    Page1 RWD

--- all.js
+++ all.js
@@ -1,17 +1,8 @@
-const swiper = new Swiper('.swiper', {
- 
-    // 分頁、左右箭頭、滾動條若有使用則必需設定          
-    // 分頁   
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    // 左右箭頭    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    // 滾動條
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+
+
+// css
+// const bi_search = document.querySelector('.bi-search');
+// const magnifier = document.querySelector('.magnifier');
+// magnifier.addEventListener('click',function(e){
+//     bi_search.setAttribute('d-none');
+// })
